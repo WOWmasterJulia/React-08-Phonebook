65b8fd73a7ab22b0c6c330fc3434c54c9808a082
WOWmasterJulia <topazzz@ukr.net>
2023-07-21 01:13:23 +0300

    Update operations.jsx

--- src/redux/operations.jsx
+++ src/redux/operations.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
 // GET @ /tasks
 export const fetchAllContacts = createAsyncThunk(
   'contacts/fetchAll',
